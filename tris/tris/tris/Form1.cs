801970675ef7659b66440f7ad7d0fa186c31fd56
Alessandro Busi <167356928+aBUSIvo00@users.noreply.github.com>
2024-10-18 22:46:51 +0200

    Update Form1.cs

--- tris/tris/tris/Form1.cs
+++ tris/tris/tris/Form1.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Drawing;
+using System.Threading;
 
 namespace tris
 {
@@ -6,6 +8,9 @@ namespace tris
     {
         private string turno;
         private int cont;
+        private int countdown;
+        private Thread threadTimer;
+
         public Form1()
         {
             InitializeComponent();
@@ -17,15 +22,15 @@ namespace tris
             if (pulsante.Text == "")
             {
                 pulsante.Text = turno;
-                timer1.Stop ();
+                timer1.Stop();
                 if (button1.Text == button2.Text && button2.Text == button3.Text && button1.Text != "" ||
-               button4.Text == button5.Text && button5.Text == button6.Text && button4.Text != "" ||
-               button7.Text == button8.Text && button8.Text == button9.Text && button7.Text != "" ||
-               button1.Text == button4.Text && button4.Text == button7.Text && button1.Text != "" ||
-               button2.Text == button5.Text && button5.Text == button8.Text && button2.Text != "" ||
-               button3.Text == button6.Text && button6.Text == button9.Text && button9.Text != "" ||
-               button1.Text == button5.Text && button5.Text == button9.Text && button1.Text != "" ||
-               button3.Text == button5.Text && button5.Text == button7.Text && button3.Text != "")
+                   button4.Text == button5.Text && button5.Text == button6.Text && button4.Text != "" ||
+                   button7.Text == button8.Text && button8.Text == button9.Text && button7.Text != "" ||
+                   button1.Text == button4.Text && button4.Text == button7.Text && button1.Text != "" ||
+                   button2.Text == button5.Text && button5.Text == button8.Text && button2.Text != "" ||
+                   button3.Text == button6.Text && button6.Text == button9.Text && button9.Text != "" ||
+                   button1.Text == button5.Text && button5.Text == button9.Text && button1.Text != "" ||
+                   button3.Text == button5.Text && button5.Text == button7.Text && button3.Text != "")
                 {
                     MessageBox.Show("Ha vinto " + turno);
                     Form1_Load(this, e);
@@ -35,22 +40,26 @@ namespace tris
                     MessageBox.Show("Pareggio");
                     Form1_Load(this, e);
                 }
+
                 if (turno == "X") turno = "0";
                 else turno = "X";
+
                 labelTurno.Text = turno;
-                timer1.Start();  
                 cont++;
-                timer1.Start();
 
-
+                if (int.TryParse(textBoxCountdown.Text, out countdown) && countdown > 0)
+                {
+                    threadTimer = new Thread(new ThreadStart(RunCountdown));
+                    threadTimer.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Inserisci un valore valido per il conto alla rovescia.");
+                }
             }
-
-
-
-            //turno = "X" ? "0" : "X";
         }
 
-        private void Form1_Load(object sender, EventArgs e) //nuova partita
+        private void Form1_Load(object sender, EventArgs e)
         {
             cont = 0;
             Random generatore = new Random();
@@ -67,17 +76,25 @@ namespace tris
             button7.Text = "";
             button8.Text = "";
             button9.Text = "";
-            timer1.Start();
+
+            textBoxCountdown.Text = ""; // Campo per inserire il conto alla rovescia
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void RunCountdown()
         {
-            timer1.Stop();
-            MessageBox.Show("Sono scaduti i 3 secondi");
-            if (turno == "X") turno = "0";
-            else turno = "X";  
-            labelTurno.Text = turno;
-            timer1.Start();
+            while (countdown > 0)
+            {
+                Thread.Sleep(1000);
+                countdown--;
+            }
+
+            this.Invoke((MethodInvoker)delegate
+            {
+                MessageBox.Show("Il tempo è scaduto!");
+                if (turno == "X") turno = "0";
+                else turno = "X";
+                labelTurno.Text = turno;
+            });
         }
     }
 }
